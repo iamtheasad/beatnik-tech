66f8c9d81b8b6a9a71d7a8ed403f3341de0bf019
iamtheasad <iamtheasad@gmail.com>
2020-10-02 21:37:35 +0600

    call to action issues fixed

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import { Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 class CallToAction extends Component {
     render() {
@@ -12,16 +12,20 @@ class CallToAction extends Component {
                         <Container>
                             <Row>
                                 <Col lg={4} md={6} sm={12} className="offset-md-5 offset-lg-8">
-                                    <h2 className="call_to_action_title">Get an instant quote from Dhaka leading insurers now.</h2>
+                                    <h2 className="call_to_action_title">Get an instant quote from Dhaka leading
+                                        insurers now.</h2>
                                     <div className="call_action_item">
-                                        <select id="cars">
-                                        <optgroup label="Select Years">
-                                            <option value="volvo">Year</option>
-                                            <option value="saab">2010</option>
-                                            <option value="saab">2011</option>
-                                            <option value="saab">2012</option>
-                                        </optgroup>
-                                    </select>
+
+                                        <label>
+                                            <select id="cars">
+                                                <optgroup label="Select Years">
+                                                    <option value="volvo">Year</option>
+                                                    <option value="saab">2010</option>
+                                                    <option value="saab">2011</option>
+                                                    <option value="saab">2012</option>
+                                                </optgroup>
+                                            </select>
+                                        </label>
                                         <select id="cars">
                                             <optgroup label="Select Years">
                                                 <option value="volvo">Brand</option>
@@ -50,6 +54,8 @@ class CallToAction extends Component {
                                         <input type="text" placeholder="Phone"/>
                                         <input type="email" placeholder="Email"/>
                                         <button type="submit">Get Quotes Now</button>
+
+
                                     </div>
                                 </Col>
                             </Row>
