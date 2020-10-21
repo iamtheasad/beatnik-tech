3f132aa7df0b9c0d43da71a841c4c89e6ea7fc1d
iamtheasad <iamtheasad@gmail.com>
2020-10-21 15:04:03 +0600

    RestApi url added

---
+++ src/RestApi/AppUrl.js
@@ -0,0 +1,18 @@
+class AppUrl {
+    static BaseUrl = 'http://127.0.0.1:8000';
+
+    static AwardData = this.BaseUrl+'/AwardData';
+    static BannerBottom = this.BaseUrl+'/BannerBottom';
+    static Banner = this.BaseUrl+'/Banner';
+    static BusinessInsurance = this.BaseUrl+'/BusinessInsurance';
+    static BannerTitle = this.BaseUrl+'/BannerTitle';
+    static Footer = this.BaseUrl+'/Footer';
+    static NewsEvent = this.BaseUrl+'/NewsEvent';
+    static OurProduct = this.BaseUrl+'/OurProduct';
+    static OurProductEtc = this.BaseUrl+'/OurProductEtc';
+    static PolicyPlan = this.BaseUrl+'/PolicyPlan';
+    static Service = this.BaseUrl+'/Service';
+    static GetQuote = this.BaseUrl+'/GetQuote';
+}
+
+export default AppUrl;
